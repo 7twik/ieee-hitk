ad3407d9210e873ccbe0d3079fe73ed5fb75ec42
7twik <sattwik.das.ece25@heritageit.edu.in>
2024-03-31 15:05:16 +0530

    removed vercel.json

--- src/app/about/Globe.jsx
+++ src/app/about/Globe.jsx
@@ -396,7 +396,7 @@ export function GlobeDemo() {
 
   return (
     <div className="flex flex-row items-center justify-center py-20 h-gl md:h-auto bg-black  relative w-full">
-      <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
+      <div className="max-w-7xl mx-auto w-full relative h-full md:h-[40rem] px-4">
         <motion.div
           initial={{
             opacity: 0,
@@ -413,7 +413,7 @@ export function GlobeDemo() {
         >
          
         </motion.div>
-        <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent to-black  z-40" />
+        <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent   z-40" />
         <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
           <World data={sampleArcs} globeConfig={globeConfig} />;
         </div>
